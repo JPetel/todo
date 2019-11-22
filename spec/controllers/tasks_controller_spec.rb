791b82ba9159e34e6cb21893d9f84b2407c734e4
JPetel <Jacob.Petel@outlook.com>
2019-11-22 15:32:05 -0500

    presenting info in console log for new text box field

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
     it "should list the tasks in the database" do
-      #task1 = FactoryBot.create(:task)
-      #task2 = FactoryBot.create(:task)
+      task1 = FactoryBot.create(:task)
+      task2 = FactoryBot.create(:task)
       task1.update_attributes(title: "Something else")
       get :index
       expect(response).to have_http_status :success
